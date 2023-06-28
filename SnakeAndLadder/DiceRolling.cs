27c2956ed18572aecf36600094dedb31fb74f47c
shrutik0501 <shrutik0501@gmail.com>
2023-06-28 14:09:27 +0530

    [Shruti]Add:Player's Position

--- SnakeAndLadder/DiceRolling.cs
+++ SnakeAndLadder/DiceRolling.cs
@@ -11,8 +11,14 @@ namespace SnakeAndLadder
         public void Game()
         {
             int position = 0;
+            int Count = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine("The dice rolled to " + dice);
+            position += dice;
             Console.WriteLine("Position of the player = " + position);
         }
     }
+    
 }
 
