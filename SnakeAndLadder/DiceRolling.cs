88814bca963a0aec73352e7195ac7d61a3ce0fdd
shrutik0501 <shrutik0501@gmail.com>
2023-06-28 13:55:53 +0530

    [Shruti]Add:Player's Position

---
+++ SnakeAndLadder/DiceRolling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class DiceRolling
+    {
+        public void Game()
+        {
+            int position = 0;
+            Console.WriteLine("Position of the player = " + position);
+        }
+    }
+}
+
---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRolling dice = new DiceRolling();
+            dice.Game();
+        }
+    }
+}
